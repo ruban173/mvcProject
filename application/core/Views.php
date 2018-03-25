4c4543007307fbfc54c135eaa733296b1dea88fc
Alexandr <ruban173@rambler.ru>
2018-03-25 20:11:57 +0300

    first commit

---
+++ application/core/Views.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ruban
+ * Date: 25.03.2018
+ * Time: 19:42
+ */
+class Views
+{
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruban
+ * Date: 25.03.2018
+ * Time: 19:12
+ */
